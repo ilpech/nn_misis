77f4d10bf7fdc2264a4ef4b3f8e48a673bbbaa84
ilpech <i.v.pichugin@yandex.ru>
2019-03-13 20:08:00 +0300

    linear sep by perceptron random center generation

--- dataset_generator.py
+++ dataset_generator.py
@@ -2,6 +2,11 @@ import matplotlib.pyplot as plt
 import sys
 import os
 import numpy as np
+import math
+
+import time
+
+
 
 center_box=(0, 100.0)
 cluster_std=2.0
@@ -9,8 +14,19 @@ n_features = 2
 n_samples = 200
 n_classes = 4
 
-def make_dataset(center_box=(0, 100.0), cluster_std=2.0,
-                 n_features = 2, n_samples = 15000, n_classes = 30,
+def euclidean_distance(first_point, second_point):
+    if first_point.shape != second_point.shape:
+        print("first_point.shape != second_point.shape")
+        return
+    sum = 0.0
+    for dim in range(len(first_point)):
+        sum += (first_point[dim] - second_point[dim])**2
+
+    return math.sqrt(sum)
+
+
+def make_dataset(center_box=(0, 100.0), cluster_std=1.0,
+                 n_features = 2, n_samples = 100000, n_classes = 100,
                  linear_separable=True):
     generator = np.random.RandomState(420)
     classes = generator.uniform(center_box[0], center_box[1], size=(n_classes, n_features))
@@ -30,7 +46,13 @@ def make_dataset(center_box=(0, 100.0), cluster_std=2.0,
             new_data = generator.normal(loc=classes[i], scale=std,
                                         size=(n, n_features))
             new_labels = np.full(n, i)
+            new_data_center = classes[i]
             for target_class in range(i):
+                target_data_center = classes[target_class]
+
+                if(euclidean_distance(target_data_center, new_data_center) > 5.0):
+                    continue
+
                 target = X[(y == target_class)]
                 target_labels = y[(y == target_class)]
                 x_ = np.concatenate((target, new_data))
@@ -42,26 +64,34 @@ def make_dataset(center_box=(0, 100.0), cluster_std=2.0,
                 perceptron = Perceptron(random_state=0)
                 perceptron.fit(x_, y_)
                 predicted = perceptron.predict(x_)
+                was_unsep = False
                 for p in range(len(predicted)):
                     if p < len(target_labels):
                         if predicted[p] == i:
                             if(linear_separable):
                                 target_labels[p] = i
-                                print(target_labels[p])
+                                was_unsep = True
                     else:
                         if predicted[p] == target_class:
                             if(linear_separable):
                                 new_labels[p - len(target_labels)] = target_class
-                                print(new_labels[p - len(target_labels)])
+                                was_unsep = True
 
-                # print(target_labels)
-                # print(new_labels)
+                if was_unsep:
+                    print((target_data_center ,new_data_center))
+                    print(euclidean_distance(target_data_center ,new_data_center))
+
                 y[(y == target_class)] = target_labels
+
+
             X = np.append(X, new_data, axis=0)
             y = np.append(y, new_labels, axis=0)
 
-    print(X.shape, y.shape)
-    plt.scatter(X[:, 0], X[:, 1], marker='o', c=y, s=25, edgecolor='k')
+    plt.scatter(X[:, 0], X[:, 1], marker='o', c=y*5, s=25, edgecolor='k')
     plt.show()
+    # print(X, y)
 
-make_dataset()
+start = time.time()
+make_dataset(linear_separable=True)
+end = time.time()
+# print(end - start)
