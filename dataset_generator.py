928dc8f62dccf327c865cb4629cd8d5e855fa322
ilpech <i.v.pichugin@yandex.ru>
2019-03-13 21:02:36 +0300

    bad commit, have to change machine

--- dataset_generator.py
+++ dataset_generator.py
@@ -25,8 +25,8 @@ def euclidean_distance(first_point, second_point):
     return math.sqrt(sum)
 
 
-def make_dataset(center_box=(0, 100.0), cluster_std=1.0,
-                 n_features = 2, n_samples = 100000, n_classes = 100,
+def make_dataset(center_box=(0, 1000.0), cluster_std=1.0,
+                 n_features = 2, n_samples = 10000, n_classes = 20,
                  linear_separable=True):
     generator = np.random.RandomState(420)
     classes = generator.uniform(center_box[0], center_box[1], size=(n_classes, n_features))
@@ -43,21 +43,28 @@ def make_dataset(center_box=(0, 100.0), cluster_std=1.0,
             y = np.append(y, np.full(n,i), axis=0)
         else:
             # check for linear sep
+
+            shift = generator.uniform(0, 5, size=(1, n_features))
+            print(shift)
+            classes[i] = classes[i-1] + shift
             new_data = generator.normal(loc=classes[i], scale=std,
                                         size=(n, n_features))
             new_labels = np.full(n, i)
             new_data_center = classes[i]
-            for target_class in range(i):
+
+            # sys.exit(0)
+            for target_class in range(i-1,i):
                 target_data_center = classes[target_class]
-
-                if(euclidean_distance(target_data_center, new_data_center) > 5.0):
-                    continue
-
+                # if(euclidean_distance(target_data_center, new_data_center) > 5.0):
+                #     continue
                 target = X[(y == target_class)]
                 target_labels = y[(y == target_class)]
+                # print(target_labels)
                 x_ = np.concatenate((target, new_data))
                 y_ = np.concatenate((target_labels, new_labels))
                 from sklearn.preprocessing import StandardScaler
+
+                # print(new_labels)
                 sc = StandardScaler()
                 x_ = sc.fit_transform(x_)
                 from sklearn.linear_model import Perceptron
@@ -65,6 +72,7 @@ def make_dataset(center_box=(0, 100.0), cluster_std=1.0,
                 perceptron.fit(x_, y_)
                 predicted = perceptron.predict(x_)
                 was_unsep = False
+                print(predicted)
                 for p in range(len(predicted)):
                     if p < len(target_labels):
                         if predicted[p] == i:
@@ -77,18 +85,38 @@ def make_dataset(center_box=(0, 100.0), cluster_std=1.0,
                                 new_labels[p - len(target_labels)] = target_class
                                 was_unsep = True
 
-                if was_unsep:
-                    print((target_data_center ,new_data_center))
-                    print(euclidean_distance(target_data_center ,new_data_center))
+                # plt.scatter(X[:, 0], X[:, 1], marker='o', c=y*5, s=25, edgecolor='k')
+                # plt.show()
 
                 y[(y == target_class)] = target_labels
 
+                print(len(new_data))
+                changed_data   = new_data[(new_labels==target_class)]
+                changed_labels = new_labels[(new_labels==target_class)]
+
+                print(len(changed_data))
+
+                X = np.append(X, changed_data, axis=0)
+                y = np.append(y, changed_labels, axis=0)
+
+                new_data = new_data[(new_labels==i)]
+                new_labels = new_labels[(new_labels==i)]
+                print(len(new_data))
+
+                # if was_unsep:
+                    # print((target_data_center ,new_data_center))
+                    # print(euclidean_distance(target_data_center ,new_data_center))
+
+                # plt.scatter(X[:, 0], X[:, 1], marker='o', c=y*5, s=25, edgecolor='k')
+                # plt.show()
 
             X = np.append(X, new_data, axis=0)
             y = np.append(y, new_labels, axis=0)
 
-    plt.scatter(X[:, 0], X[:, 1], marker='o', c=y*5, s=25, edgecolor='k')
-    plt.show()
+        plt.scatter(X[:, 0], X[:, 1], marker='o', c=y*5, s=25, edgecolor='k')
+        plt.show()
+
+
     # print(X, y)
 
 start = time.time()
